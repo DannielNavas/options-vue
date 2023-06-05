e8fd8c281ae23487b9eca38753271023810e68f3
DannielNavas <dannielnavas@gmail.com>
2023-06-05 08:47:06 -0500

    ✨ ciclo de vida

--- README.md
+++ README.md
@@ -1,24 +1,49 @@
 # options
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Lints and fixes files
+
 ```
 yarn lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Ciclo de vida de los componentes
+
+Ciclo de vida de los componentes, así tambien se sus hooks las cuales son funciones que nos permiten definir acciones en cada etapa del ciclo de los componentes
+
+### beforeCreate.
+
+### created.
+
+### beforeMount.
+
+### mounted.
+
+### beforeUpdate.
+
+### updated.
+
+### beforeUnmount.
+
+### unmounted.
---
+++ src/App-ciclo-de-vida.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>{{ text }}</div>
+  <button @click="update">cambiar</button>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      text: 'Hello World',
+    };
+  },
+  beforeCreate() {
+    console.log('beforeCreate', this.$data, this.$el);
+  },
+  created() {
+    console.log('created', this.$data, this.$el);
+  },
+  mounted() {
+    console.log('mounted', this.$data, this.$el);
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate', this.$data, this.$el);
+  },
+  updated() {
+    console.log('updated', this.$data, this.$el);
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount', this.$data, this.$el);
+  },
+  unmounted() {
+    console.log('unmounted', this.$data, this.$el);
+  },
+  methods: {
+    update() {
+      this.text = 'Hello World 2';
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
