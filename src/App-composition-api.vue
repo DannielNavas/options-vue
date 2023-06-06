a5f07bf556875f53ec3fbe6699630c8d5fac1189
DannielNavas <dannielnavas@gmail.com>
2023-06-06 08:19:42 -0500

    ✨ composition api

---
+++ src/App-composition-api.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>{{ text }}</div>
+  <UserRepositories :user="'DannielNavas'" />
+</template>
+
+<script>
+import UserRepositories from './components/UserRepositories.vue';
+
+export default {
+  name: 'App',
+  components: {
+    UserRepositories,
+  },
+  data() {
+    return {
+      text: 'Hello World',
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/UserRepositories.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>{{ user }} repositories</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'UserRepositories',
+  props: {
+    user: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props, ctx) {
+    console.log(props);
+    console.log(ctx);
+    return {
+      repositories: [],
+      filters: {},
+      searchQuery: '',
+    };
+  },
+  data() {
+    // return {
+    //   repositories: [],
+    //   filters: {},
+    //   searchQuery: '',
+    // };
+  },
+  computed: {
+    filteredRepositories() {
+      return null;
+    },
+
+  },
+};
+</script>
