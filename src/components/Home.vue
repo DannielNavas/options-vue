90f40f3f6e646c41dbd32ec756511b0a07eaecdb
DannielNavas <dannielnavas@gmail.com>
2023-06-06 08:34:43 -0500

    ✨ ref, reactive

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,28 @@
 <template>
-  <div>Hola</div>
+  <div>{{ counter }}</div>
+  <div>Home {{ obj }}</div>
 </template>
 
 <script>
-import { onMounted } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 
 export default {
   setup() {
     onMounted(() => {
       console.log('mounted');
     });
-    return {};
+    const counter = ref(0);
+    const obj = reactive({ counter: 0 });
+
+    setInterval(() => {
+      obj.counter += 1;
+      counter.value += 1;
+    }, 1000);
+
+    return {
+      counter,
+      obj,
+    };
   },
 };
 </script>
